765493412a62cf454e3a8045831ca408b0c7030e
Yaroslava Zharova <zharova.yaroslava.ya@yandex.ru>
2023-06-10 22:53:32 +0300

    исправления

--- admin_test.py
+++ admin_test.py
@@ -10,16 +10,14 @@ password = driver.find_element(By.NAME, "password").send_keys("admin")
 login_button = driver.find_element(By.NAME, "login").click()
 
 nav_list = driver.find_elements(By.CSS_SELECTOR, "#box-apps-menu li")
-for i in len(nav_list):
-    driver.find_element(By.CSS_SELECTOR, "#box-apps-menu > li:nth-child(" + i+")").click()
-    sub_elements = driver.find_elements(By.CSS_SELECTOR, "#box-apps-menu > li:nth-child(" + i +") li")
+
+for i in range(1,len(nav_list)+1):
+    driver.find_element(By.CSS_SELECTOR, f"#box-apps-menu  li:nth-child({i})").click()
+    sub_elements = driver.find_elements(By.CSS_SELECTOR, f"#box-apps-menu li:nth-child({i}) li")
     if len(sub_elements) != 0:
-        a = 1
-        while a <= len(sub_elements):
-            driver.find_element(By.CSS_SELECTOR, "#box-apps-menu > li:nth-child(" + i + ") > ul > li:nth-child(" + a + ")").click()
+        for a in range(1,len(sub_elements)+1):
+            driver.find_element(By.CSS_SELECTOR, f"#box-apps-menu >li:nth-child({i}) ul li:nth-child({a})").click()
             driver.find_element(By.CSS_SELECTOR, "h1")
-
-
 driver.close()
 driver.quit()
 
